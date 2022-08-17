73113cceefc6a7b90da8deb78fc0edafae745e9f
Júlia Bandeira <juliabandeira2004@gmail.com>
2022-08-17 14:36:49 -0300

    cadastro-de-eventos.js

---
+++ cadastro-de-eventos.js
@@ -0,0 +1,27 @@
+const dataAtual = 17
+
+console.log ("Olá, seja bem vindo(a)! Para começar, informe a data do evento:")
+let dataDoEvento = 16
+if (dataDoEvento > dataAtual) {console.log("Evento permitido!")}
+else (console.log("O cadastro não será permitido por data inválida."))
+
+console.log("-------------------------------")
+
+console.log("Informe a sua idade:")
+let idadeReal = 11
+if (idadeReal >= 18) {console.log("Idade permitida!")}
+else (console.log("O cadastro não será permitido por idade insuficiente."))
+
+console.log("-------------------------------")
+
+console.log("Lista de participantes:")
+let participantes = 100
+if (participantes < 100) {console.log("Quaantidade de participantes permitida!")}
+else (console.log ("O cadastro não será permitido por limite excedido de participantes no evento."))
+
+console.log("-------------------------------")
+
+
+
+
+
